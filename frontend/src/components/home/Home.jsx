6829210f834e3bd4a7bde0888c447575d9a57062
Mr. Santosh Kumar Gupta <87138199+Sainkee@users.noreply.github.com>
2024-08-12 17:06:08 +0530

    Update Home.jsx

--- frontend/src/components/home/Home.jsx
+++ frontend/src/components/home/Home.jsx
@@ -32,7 +32,7 @@ export default function Home() {
           className="bg-red-600 text-white py-2 px-4 rounded-md"
           to="/login"
         >
-          Sing In
+          Sign In
         </Link>
       </header>
 
